d28dfae03dbe2e3e8cc8454df1616e83b65dd33a
Matt Layman <matthewlayman@gmail.com>
2019-06-20 23:26:21 -0400

    Add purgecss.

--- themes/tailwind-site/postcss.config.js
+++ themes/tailwind-site/postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './layouts/**/*.html',
+  ],
+
+  // Overriding the default extractor because the default doesn't consider colon
+  // and Tailwind makes heavy use of that as a class name.
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss,
   ]
 }
