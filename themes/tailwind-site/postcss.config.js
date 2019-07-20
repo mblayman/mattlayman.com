47e65471df2246ad74174fcef937dfeffafb4f52
Matt Layman <matthewlayman@gmail.com>
2019-07-20 15:49:51 -0400

    Minify CSS since Netlify doesn't seem to be doing that.

--- themes/tailwind-site/postcss.config.js
+++ themes/tailwind-site/postcss.config.js
@@ -9,10 +9,15 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
+const cssnano = require('cssnano')({
+  preset: 'default',
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     purgecss,
+    cssnano,
   ]
 }
