cf9f6fb1cc6661bdc1a66bbd555fb0c044256826
Matt Layman <matthewlayman@gmail.com>
2020-07-19 01:12:54 -0400

    Add a Checklist class to store checklist progress.

--- bin/checklist.py
+++ bin/checklist.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python
 
+import json
 from pathlib import Path
 
 import typer
+
+ROOT_DIR = Path(__file__).parent.parent
+CHECKLIST_ITEMS = ("dev",)
 
 
 def main(article: Path = typer.Argument(..., exists=True)):
     if article.suffix != ".md":
         typer.echo("The provided article path is not a Markdown file.")
         raise typer.Exit(code=1)
+
+    checklist = Checklist(article)
+    checklist.load(CHECKLIST_ITEMS)
+
+    # TODO: Use the checklist.
+
+    checklist.write()
+    print(checklist.items)
+
+
+class Checklist:
+    """A container to track which checklist items are processed."""
+
+    checklists_dir = ROOT_DIR / "checklists"
+
+    def __init__(self, article):
+        self.article = article
+
+    @property
+    def path(self):
+        article_path = str(self.article.relative_to("content").with_suffix(".json"))
+        article_path = article_path.replace("/", "_")
+        return self.checklists_dir / article_path
+
+    def load(self, checklist_items):
+        try:
+            with self.path.open() as f:
+                self.items = json.load(f)
+        except FileNotFoundError:
+            self.items = {item: False for item in checklist_items}
+
+    def write(self):
+        """Store the checklist for any future use."""
+        with self.path.open("w") as f:
+            json.dump(self.items, f, sort_keys=True, indent=2)
 
 
 if __name__ == "__main__":
