36d5824e7e285f1a6267e1caa823a2d0a4f4d189
Matt Layman <matthewlayman@gmail.com>
2023-05-20 15:45:33 -0400

    Start video poster workflow.

---
+++ bin/video-poster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Check for any videos from my channel and auto-generate the content post.
+
+# TODO: set the bash mode to fail properly.
+
+# 1. Look for new content and generate a post if found
+# 2. Run hugo to make sure it builds.
