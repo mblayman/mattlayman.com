f01f9805d3764eb1e05967e852fc67e150cf73f1
Matt Layman <matthewlayman@gmail.com>
2023-05-20 12:30:47 -0400

    Cleanup stuff that I don't use anymore.

--- bin/cli.py
+++ bin/cli.py
@@ -1,6 +1,5 @@
 import typer
 
-import checklist
 import create_campaign
 import make_youtube_article
 from tools import newsletter
@@ -8,6 +7,5 @@ from tools import newsletter
 app = typer.Typer()
 
 app.command("campaign")(create_campaign.main)
-app.command("checklist")(checklist.main)
 app.command("newsletter")(newsletter.main)
 app.command("yt")(make_youtube_article.main)
