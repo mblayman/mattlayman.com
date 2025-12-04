5587e0bf57d032d07446abb70d893f5ce645116e
Matt Layman <matthewlayman@gmail.com>
2025-12-04 00:29:27 -0500

    Add an /up route for kamal healthcheck.

--- app/entry.lua
+++ app/entry.lua
@@ -1,10 +1,16 @@
 local Application = require("nibiru.application")
+local http = require("nibiru.http")
 local Route = require("nibiru.route")
 
 local resp = require("app.responders")
 
+function up(request)
+    return http.ok("")
+end
+
 local routes = {
     Route("/", resp.index),
+    Route("/up", up),
 }
 
 return Application(routes)
